cd99718c3e7ebea4ed7ee3d556acf16a665a5ba2
Peter Green <peter.green@dachisgroup.com>
2014-05-20 13:49:37 +0100

    Initial commit: new cookbook created with knife.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'steamcmd'
+maintainer       'Dachis Group'
+maintainer_email 'eu.support+chef@dachisgroup.com'
+license          'All rights reserved'
+description      'Installs/Configures steamcmd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: steamcmd
+# Recipe:: default
+#
+# Copyright 2014, Dachis Group
+#
+# All rights reserved - Do Not Redistribute
+#
