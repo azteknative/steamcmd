921626f4a68618f371f680215515dde27f14332a
Peter Green <peter.green@dachisgroup.com>
2014-06-09 14:59:40 +0100

    Changed the author info and coded retrieval of Steam client and extract it.

---
+++ attributes/default.rb
@@ -0,0 +1,12 @@
+
+# User and group to install SteamCMD as (will be created
+# if necessary)
+default[:steamcmd][:user] = "steam"
+default[:steamcmd][:group] = "games"
+
+# Any additional users that should be added to the above group
+default[:steamcmd][:additional_users] = []
+
+# Install path
+default[:steamcmd][:install_path] = "/usr/local/games/steamcmd"
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,41 @@
 # Cookbook Name:: steamcmd
 # Recipe:: default
 #
-# Copyright 2014, Dachis Group
+# Copyright 2014, AzTek Native Ltd.
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Create user and group
+group node[:steamcmd][:group] do
+  action :create
+  if node[:steamcmd][:additional_users].length < 1
+    members node[:steamcmd][:additional_users]
+    append true
+  end
+end
+
+user node[:steamcmd][:user] do
+  action :create
+  gid node[:steamcmd][:group]
+end
+
+# Retrieve steamcmd archive
+remote_file "/tmp/steamcmd_linux.tar.gz" do
+  source "http://media.steampowered.com/installer/steamcmd_linux.tar.gz"
+end
+
+directory node[:steamcmd][:install_path] do
+  action :create
+  owner node[:steamcmd][:user]
+  group node[:steamcmd][:group]
+  recursive true
+end
+
+bash "extract_steamcmd" do
+  code "tar xzf /tmp/steamcmd_linux.tar.gz -C #{node[:steamcmd][:install_path]}"
+  user node[:steamcmd][:user]
+end
+
+
+
