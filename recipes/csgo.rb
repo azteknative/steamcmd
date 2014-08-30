7e71ecac21d9628b6a0a4c1356e59671e156f337
Peter Green <peter.green@az-tek.co.uk>
2014-08-30 12:13:09 +0100

    Added steamcmd::csgo recipe - installs CSGO dedicated server via the LWRP.

---
+++ recipes/csgo.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: steamcmd
+# Recipe:: default
+#
+# Copyright 2014, AzTek Native Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
+steamcmd_app "csgo" do
+  action :install
+  app_id 740
+end
+
