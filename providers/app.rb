25db996b98d0accb51d520626677ad93f0ba03c7
Peter Green <peter.green@az-tek.co.uk>
2014-09-08 14:34:44 +0100

    Added GPLv2 boilerplate to source files.

--- providers/app.rb
+++ providers/app.rb
@@ -1,4 +1,26 @@
-
+#
+# Cookbook Name:: steamcmd
+# Provider:: app
+#
+# Author: Peter Green <peter.green@az-tek.co.uk>
+#
+# Copyright (C) 2014 AzTek Native Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#
 action :install do
 
   directory "#{new_resource.base_path}/#{new_resource.name}" do
